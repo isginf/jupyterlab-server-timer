ee5cbf0a92772dff68cc87f11ef1233a000b8773
Stefan Walter <stefan.walter@inf.ethz.ch>
2024-02-09 14:38:26 +0100

    Testing

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,13 @@
 import {
   JupyterFrontEnd,
-  JupyterFrontEndPlugin
+  JupyterFrontEndPlugin,
 } from '@jupyterlab/application';
 
-import {
-  IToolbarWidgetRegistry,
-} from '@jupyterlab/apputils';
+import { IStatusBar } from '@jupyterlab/statusbar';
 
 import { Widget } from '@lumino/widgets';
 
-import { ISettingRegistry } from '@jupyterlab/settingregistry';
-
 import '../style/index.css';
-
-const SERVER_TIMER = 'jp-Server-Timer';
 
 /*const TOPBAR_TIMER_RED = 'jp-Timer-Red';*/
 
@@ -24,24 +18,26 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-server-timer:plugin',
   description: 'A JupyterLab extension that displays the remaining server run time in the top bar.',
   autoStart: true,
-  requires: [IToolbarWidgetRegistry],
+  requires: [IStatusBar],  
   activate: async (
     app: JupyterFrontEnd,
-    toolbarRegistry: IToolbarWidgetRegistry
+    statusBar: IStatusBar
   ): Promise<void> => {
-    console.log('JupyterLab extension jupyterlab-server-timer is activated!');
-
-    var timer = 30;
 
     const textNode = document.createElement('div');
     textNode.textContent = "xxx";
 
-    toolbarRegistry.addFactory('ServerTimer', 'notification', () => {
-      const textWidget = new Widget({ node: textNode });
-      textWidget.addClass(TOPBAR_TEXT);
-      return textWidget;
-    });
+    const statusWidget = new Widget({ node: textNode });
 
+    statusBar.registerStatusItem('lab-status', {
+      align: 'middle',
+      item: statusWidget
+    });
+    
+    console.log('JupyterLab extension jupyterlab-server-timer is activated!');
+
+    var timer = 30;
+    
     setInterval(() => {
       timer = timer - 1;
       textNode.textContent = timer.toString();
