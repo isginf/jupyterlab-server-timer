5cc0f38ed950b2addae09507b62e43b8aa0f03d3
Stefan Walter <stefan.walter@inf.ethz.ch>
2024-02-12 09:39:26 +0100

    Testing

--- jupyterlab_server_timer/handlers.py
+++ jupyterlab_server_timer/handlers.py
@@ -8,20 +8,15 @@ import tornado
 start_time = datetime.now()
 
 class RouteHandler(APIHandler):
-    # The following decorator should be present on all verb methods (head, get, post,
-    # patch, put, delete, options) to ensure only authorized user can request the
-    # Jupyter server
     @tornado.web.authenticated
     def get(self):
         self.finish(json.dumps({
             "comment": "Server life span.",
-            "start": start_time
+            "server-start": int(start_time.timestamp())
         }))
-
 
 def setup_handlers(web_app):
     host_pattern = ".*$"
-
     base_url = web_app.settings["base_url"]
     route_pattern = url_path_join(base_url, "jupyterlab-server-timer", "get-life-span")
     handlers = [(route_pattern, RouteHandler)]
